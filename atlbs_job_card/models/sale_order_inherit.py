8328c74f77a529244db34e9bb2ca87cd08d42212
atlabs <merlinthomasofficial@gmail.com>
2025-08-26 12:16:38 +0400

    changes in the sale form

--- atlbs_job_card/models/sale_order_inherit.py
+++ atlbs_job_card/models/sale_order_inherit.py
@@ -5,6 +5,8 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    job_card_id = fields.Many2one("job.card.management", string="Job Card")
+
     def action_add_vehicle_products(self):
         return {
             'name': 'Add Vehicle Products',
@@ -17,6 +19,42 @@ class SaleOrder(models.Model):
 
 
 
+# function commented on aug26
+    # def action_create_job_card(self):
+    #     self.ensure_one()
+    #     job_card = self.env['job.card.management'].create({
+    #         'sale_order_id': self.id,
+    #         'partner_id': self.partner_id.id,
+    #         'email': self.partner_id.email,
+    #         'phone': self.partner_id.phone,
+    #         'whatsapp_no': self.partner_id.whatsapp_no,
+    #         'state': 'memo',
+    #         # 'register_no': vehicle_make_id,
+    #
+    #     })
+    #     job_card_line_obj = self.env['job.card.line']
+    #     for line in self.order_line:
+    #         job_card_line_obj.create({
+    #             'job_card_id': job_card.id,
+    #             'department': line.department,
+    #             'product_template_id': line.product_template_id.id if hasattr(line, 'product_template_id') else False,
+    #             'description': line.name,
+    #             'price_unit': line.price_unit,
+    #             'quantity': line.product_uom_qty,
+    #             'tax_ids': [(6, 0, line.tax_id.ids)] if hasattr(line, 'tax_id') else False,
+    #             'discount': line.discount,
+    #             'after_discount': line.price_unit * (1 - (line.discount or 0.0) / 100),
+    #             # compute tax_amount if you want here
+    #             'uom': line.product_uom.id if hasattr(line, 'product_uom') else False,
+    #         })
+    #     return {
+    #         'name': 'Job Card',
+    #         'type': 'ir.actions.act_window',
+    #         'res_model': 'job.card.management',
+    #         'view_mode': 'form',
+    #         'res_id': job_card.id,
+    #         'target': 'current',
+    #     }
 
     def action_create_job_card(self):
         self.ensure_one()
@@ -27,9 +65,10 @@ class SaleOrder(models.Model):
             'phone': self.partner_id.phone,
             'whatsapp_no': self.partner_id.whatsapp_no,
             'state': 'memo',
-            # 'register_no': vehicle_make_id,
-
         })
+        # link back to sale order
+        self.job_card_id = job_card.id
+
         job_card_line_obj = self.env['job.card.line']
         for line in self.order_line:
             job_card_line_obj.create({
@@ -42,7 +81,6 @@ class SaleOrder(models.Model):
                 'tax_ids': [(6, 0, line.tax_id.ids)] if hasattr(line, 'tax_id') else False,
                 'discount': line.discount,
                 'after_discount': line.price_unit * (1 - (line.discount or 0.0) / 100),
-                # compute tax_amount if you want here
                 'uom': line.product_uom.id if hasattr(line, 'product_uom') else False,
             })
         return {
@@ -53,7 +91,6 @@ class SaleOrder(models.Model):
             'res_id': job_card.id,
             'target': 'current',
         }
-
 
 
 class SaleOrderLine(models.Model):
