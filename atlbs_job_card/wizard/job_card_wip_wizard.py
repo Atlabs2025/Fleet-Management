cdcd3ed9b2b04be11893d7d78f1a1b9281cce2ca
atlabs <merlinthomasofficial@gmail.com>
2025-09-26 15:33:00 +0400

    changes in the report wip

--- atlbs_job_card/wizard/job_card_wip_wizard.py
+++ atlbs_job_card/wizard/job_card_wip_wizard.py
@@ -6,14 +6,6 @@ class JobCardWIPWizard(models.TransientModel):
 
     date_from = fields.Date(string="Date From", required=True)
     date_to = fields.Date(string="Date To", required=True)
-    # due_days_label = fields.Char(string="Due Days Label")
-
-    # def action_print_report(self):
-    #     self.ensure_one()
-    #     return self.env.ref('atlbs_job_card.action_report_job_card_not_completed').report_action(self, data={
-    #         'date_from': self.date_from,
-    #         'date_to': self.date_to
-    #     })
 
     def action_print_report(self):
         self.ensure_one()
@@ -26,5 +18,36 @@ class JobCardWIPWizard(models.TransientModel):
             job_cards
         )
 
+    # def action_view_report(self):
+    #     self.ensure_one()
+    #     job_cards = self.env['job.card.management'].search([
+    #         ('state', '!=', 'completed'),
+    #         ('created_datetime', '>=', self.date_from),
+    #         ('created_datetime', '<=', self.date_to),
+    #     ])
+    #     action = {
+    #         'type': 'ir.actions.act_window',
+    #         'name': 'WIP Job Cards',
+    #         'view_mode': 'tree,form',
+    #         'res_model': 'job.card.management',
+    #         'domain': [('id', 'in', job_cards.ids)],
+    #         'target': 'current',
+    #     }
+    #     return action
 
-
+    def action_view_report(self):
+        """Open WIP Job Cards in list view"""
+        self.ensure_one()
+        job_cards = self.env['job.card.management'].search([
+            ('state', '!=', 'completed'),
+            ('created_datetime', '>=', self.date_from),
+            ('created_datetime', '<=', self.date_to),
+        ])
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'WIP Job Cards',
+            'res_model': 'job.card.management',
+            'view_mode': 'list,form',
+            'domain': [('id', 'in', job_cards.ids)],
+            'target': 'current',
+        }
